43e98e45a60244ac9d2aa39c76fe07823092802d
nili-git <nili7248@gmail.com>
2022-01-24 16:28:44 +0545

    master

---
+++ Test 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = "San";
+            Console.WriteLine("Hello World!");
+            var number = Int32.Parse(Console.ReadLine());
+            
+        
+        
+        }
+    }
+}
